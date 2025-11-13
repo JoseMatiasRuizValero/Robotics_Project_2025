5ea31801e12cba31115d564eca3c4f7b33468eb1
Jia En Lu <JXL1621@student.bham.ac.uk>
2025-11-13 18:20:43 +0000

    fix: robot keeps stopping, added extra penalty

--- utils/rewards.py
+++ utils/rewards.py
@@ -3,7 +3,7 @@ import math
 def get_distance(pos1, pos2):
     return math.sqrt((pos1[0]-pos2[0])**2 + (pos1[1]-pos2[1])**2)
 
-def calculate_reward(robot_pos, goal_pos, sensors):
+def calculate_reward(robot_pos, goal_pos, sensors, action):
     dist = get_distance(robot_pos, goal_pos)
 
     # reached goal
@@ -15,4 +15,10 @@ def calculate_reward(robot_pos, goal_pos, sensors):
         return -50, True
 
     # each step costs -1
-    return -1, False
+    reward = -1
+
+    # discourage stopping - robot was stopping too much in tests
+    if action == 4:
+        reward -= 2  # tried -1 first but still stopped a lot
+
+    return reward, False
